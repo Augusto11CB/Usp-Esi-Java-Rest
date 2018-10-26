b0cb2aac898f938d0962a816a11b457bb512bfbe
AugustoCalado <augusto.bueno@usp.br>
2018-10-25 21:36:41 -0300

    Spring Boot Simple Funfanfo

---
+++ demo/src/main/java/com/erudia/demo/resource/FiltroResource.java
@@ -0,0 +1,34 @@
+package com.erudia.demo.resource;
+
+import com.FiltroAndReco.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/filtros") // "/buscar"
+public class FiltroResource{
+
+    //@Autowired
+    //private Filtro f;
+    private Buscar b;
+
+//    @PostMapping
+//http://localhost:8080/filtros/coisa
+    @GetMapping("/{buscar}")
+    public String buscar(@PathVariable String buscar){
+        b = new Buscar();
+        return b.relacionados(buscar);
+    }
+
+    
+    
+
+}
---
+++ demo/src/main/java/com/erudia/demo/resource/RequisicaoResource.java
@@ -0,0 +1,41 @@
+// package com.augusto.augs.resource;
+
+// import java.util.HashMap;
+
+// import javax.annotation.Generated;
+
+// import com.FiltroAndReco.*;
+
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+
+// @RestController
+// @RequestMapping("/requisitar")
+// public class RequisicaoResource {
+
+//     @GetMapping("/busca/{nomeProduto}")
+//     public HashMap<String, Object> buscar(@PathVariable String nomeProduto) {
+//         HashMap<String, Object> map = new HashMap<>();
+//         Buscar algDeBusca = new Buscar();
+//         return null;
+//     }
+
+//     @GetMapping("/maispop")
+//     public HashMap<String, Object> buscarMaisPopulares() {
+//         HashMap<String, Object> map = new HashMap<>();
+//         return null;
+//     }
+
+//     @@GetMapping("/cliente/{id}")
+//     public HashMap<String, Object> getMethodName(@PathVariable int id) {
+//         return null;
+//     }
+    
+
+
+
+// }
