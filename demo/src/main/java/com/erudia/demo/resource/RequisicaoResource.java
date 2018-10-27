6f7dee110cf5b6c62cbfd3ff5485c8a5a881d0b1
AugustoCalado <augusto.bueno@usp.br>
2018-10-26 23:03:12 -0300

    Criacao de instancias pythons para atender as requizições dos diferentes gets existentes em nossa api

--- demo/src/main/java/com/erudia/demo/resource/RequisicaoResource.java
+++ demo/src/main/java/com/erudia/demo/resource/RequisicaoResource.java
@@ -17,21 +17,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/requisitar")
 public class RequisicaoResource {
 
+    private IndexMannager mIndexBuscar = new IndexMannager(7000, 101);
+    private IndexMannager mIndexBuscarMaisPopulares = new IndexMannager(7103, 101);
+    private IndexMannager mIndexRecomendacao = new IndexMannager(7208, 101);
+
+  //  String resp = JythonComunication.SendToPython(mIndex, fala);
+
     @GetMapping("/busca/{pesquisa}/{categoria}/{valormin}/{valormax}/{ordenacao}")
     public HashMap<String, Object> buscar(@PathVariable String pesquisa, @PathVariable String categoria, @PathVariable String valormin, @PathVariable String valormax, @PathVariable String ordenacao ) {
+        //TODO Parsear Categoria 
+        //String entradaDaBusca = "camisa,masculina|moderna|nike,0,2,DESC"
+        String entradaDaBusca = pesquisa  + "," + categoria + "," + valormin + "," + valormax + "," + ordenacao;
+        String resp = JythonComunication.SendToPython(mIndexBuscar, entradaDaBusca);
         HashMap<String, Object> map = new HashMap<>();
-        Buscar algDeBusca = new Buscar();
         return null;
     }
 
     @GetMapping("/maispop")
     public HashMap<String, Object> buscarMaisPopulares() {
+        String respostaDosPopulares = JythonComunication.SendToPython(mIndexBuscarMaisPopulares, "");
         HashMap<String, Object> map = new HashMap<>();
         return null;
     }
 
-    @GetMapping("/cliente/{id}")
-    public HashMap<String, Object> recomendacao(@PathVariable int id) {
+    @GetMapping("/cliente/{idCliente}")
+    public HashMap<String, Object> recomendacao(@PathVariable int idCliente) {
+        String recomendacaoParaCliente = JythonComunication.SendToPython(mIndexRecomendacao, "" + idCliente);
         return null;
     }
     
